03556c61453bc93cec5606d05ebb783269f012dc
Goodness Nwankwo (NDER) <126233126+NdersCode@users.noreply.github.com>
2023-09-20 07:47:50 +0100

    Adding onTap to the categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:meals/data/dummy_data.dart';
+import 'package:meals/screens/meals.dart';
 import 'package:meals/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const MealsScreen(
+          title: 'Some Title...',
+          meals: [],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,13 +27,18 @@ class CategoriesScreen extends StatelessWidget {
         padding: const EdgeInsets.all(16),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
-          childAspectRatio: 3 / 3,
+          childAspectRatio: 3 / 2,
           crossAxisSpacing: 20,
           mainAxisSpacing: 20,
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectedCategory: () {
+                _selectCategory(context);
+              },
+            ),
         ],
       ),
     );
