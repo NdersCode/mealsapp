037776e25dfcbc7355e4d8d17cc42d7a50bd13d0
Goodness Nwankwo (NDER) <126233126+NdersCode@users.noreply.github.com>
2023-09-25 08:15:57 +0100

    Improving the meals_details Screen

--- lib/screens/meal_details.dart
+++ lib/screens/meal_details.dart
@@ -14,14 +14,76 @@ class MealDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(title),
-        ),
-        body: Image.network(
-          meal.imageUrl,
-          height: 300,
-          width: double.infinity,
-          fit: BoxFit.fill,
-        ));
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            meal.imageUrl,
+            height: 300,
+            width: double.infinity,
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 32,
+          ),
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              children: [
+                Text(
+                  'Ingredients',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      letterSpacing: 1.2),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                for (final ingredient in meal.ingredients)
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 4),
+                    width: double.infinity,
+                    child: Text(
+                      ingredient,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                const SizedBox(
+                  height: 32,
+                ),
+                Text(
+                  'Steps',
+                  style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                      color: Theme.of(context).colorScheme.primary,
+                      letterSpacing: 1.2),
+                  textAlign: TextAlign.left,
+                ),
+                const SizedBox(
+                  height: 8,
+                ),
+                for (final step in meal.steps)
+                  Container(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    width: double.infinity,
+                    child: Text(
+                      '• ' + step,
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.onBackground,
+                          ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
